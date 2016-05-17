cd13cbec68d8e92e878ea9ea9578b260e71a7961
Hanson-Hao-Wu <hanson.hao.wu@gmail.com>
2016-05-17 15:29:16 -0400

    first time

---
+++ src/main/java/org/hao/text01/action/UserServlet.java
@@ -0,0 +1,51 @@
+package org.hao.text01.action;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class UserServlet
+ */
+public class UserServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public UserServlet() {
+        super();
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		//response.getWriter().append("Served at: ").append(request.getContextPath());
+		request.setCharacterEncoding("UTF-8");
+		response.setContentType("text/html;charset=utf-8");
+		
+		String action = request.getParameter("action");
+		if("login_input".equals(action)) {
+			request.getRequestDispatcher("login.jsp").forward(request , response);
+		} else if("login".equals(action)) {
+			String name = request.getParameter("name");
+			String password = request.getParameter("password");
+			
+			System.out.println("name->" + name + ",password->" + password);
+		}
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		doGet(request, response);
+	}
+
+}
